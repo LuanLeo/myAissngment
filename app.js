ee23012290e5f77b0645045115dc74eb3ea95a9b
LuanLeo <97430056+LuanLeo@users.noreply.github.com>
2022-03-06 13:34:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var Database = require('./db/database');
 var routes = require('./routes/controller');
 var app = express();
+const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -11,6 +12,6 @@ app.set('views', './views');
 // Website routes
 app.use('/', routes);
 
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log("Starting at port 3000...");
 });
